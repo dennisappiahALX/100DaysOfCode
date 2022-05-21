9b1e67fe1f81e217292df2eeee876e4c5b9d4549
dennisappiahALX <appiahdennis1998@gmail.com>
2022-05-21 15:33:19 +0000

    loop repeat ten times

---
+++ Round 1/loops_repeat.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+    for (int j = 0; j <= 9; j++)
+    {
+        for (int i =0; i <= 5; i++)
+        {
+            printf("%d", i);
+        }
+        printf("\n");
+    }
+    getch();
+}
